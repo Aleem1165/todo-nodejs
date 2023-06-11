2f4f619d04c25c559f13653066849ae2bdbd21cd
Aleem1165 <abdulaleem8021@gmail.com>
2023-06-11 23:14:04 +0500

    todo-nodejs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { RotatingLines } from "react-loader-spinner";
 
 function App() {
+  const backendURL = "http://localhost:5000/apis/";
+
+  const [text, setText] = useState("");
+  const [todoData, setTodoData] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(backendURL + "todo/read");
+      console.log(data.data);
+      setTodoData(data.data);
+    })();
+  }, []);
+
+  const handleAdd = async () => {
+    const { data } = await axios.post(backendURL + "todo/create", {
+      inputData: text,
+      text,
+    });
+    setTodoData(data.data)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {todoData ? (
+        <div className="App">
+          <input
+            type={"text"}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={handleAdd}>add</button>
+          <div>
+            {todoData.map((item, index) => {
+              return <p key={index}>{item.text}</p>;
+            })}
+          </div>
+        </div>
+      ) : (
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      )}
     </div>
   );
 }
